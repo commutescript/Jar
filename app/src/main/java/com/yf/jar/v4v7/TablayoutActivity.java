7b6fa7f7ed46eb83a4674205ee4b074c001d1052
commutescript <727030302@qq.com>
2017-06-15 15:43:11 +0800

    平时练习各个知识点的demo

---
+++ app/src/main/java/com/yf/jar/v4v7/TablayoutActivity.java
@@ -0,0 +1,15 @@
+package com.yf.jar.v4v7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yf.jar.R;
+
+public class TablayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablayout);
+    }
+}
